c265fd7d026f3f370238cefab8f63aede581ca55
namho102 <namhoang.td09@gmail.com>
2016-04-11 14:27:05 +0100

    add middlewares

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var express = require("express");
 var cors = require("cors");
 var morgan = require("morgan");
+var compression = require("compression");
+var https = require("https");
+var fs = require("fs");
+var helmet = require("helmet");
 var app = express();
+
 
 //generating logs
 app.use(morgan("common"));
+
+//armoring the API with Helmet
+app.use(helmet());
 
 //only allow client apps from the address: localhost:3001
 app.use(cors({
@@ -13,11 +21,27 @@ app.use(cors({
 	allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
+//compacting requests using GZIP middleware
+app.use(compression());
+
 app.get("/", function(req, res) {
 	res.json({
 		status: "My API is alive!"
 	});
 });
+
+/*
+//installing SSL support to use HTTPS
+var credentials = {
+	key: fs.readFileSync("my-api.key", "utf8"),
+	cert: fs.readFileSync("my-api.cert", "utf8")
+};
+https
+	.createServer(credentials, app)
+	.listen(3000, function() {
+		console.log("My API is running...");
+	});
+*/
 
 app.listen(3000, function() {
 	console.log("My API is running...");
